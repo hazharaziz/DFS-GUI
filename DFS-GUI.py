fd72ac4d36046c87b838ba4673fecbf3fd1acc96
Hazhar Aziz <hazharaziz5555@gmail.com>
2019-06-13 18:25:06 +0430

    First Commit

---
+++ DFS-GUI.py
@@ -0,0 +1,12 @@
+# import matplotlib.pyplot as plt
+# import seaborn as sns
+
+
+# x = [1,2,3,4,5,6]
+# y = [5,6,1,2,4,6]
+
+# ax = sns.stripplot(x,y)
+# ax.set(xlabel = 'X axis', ylabel = 'Y axis')
+
+# plt.title('My first graph')
+# plt.show()
