76d49e7329ca7ea814bb9afbd14cf285f928e30b
Hazhar Aziz <hazharaziz5555@gmail.com>
2019-06-17 20:22:14 +0430

    Second Commit

--- DFS-GUI.py
+++ DFS-GUI.py
@@ -1,12 +1,12 @@
-# import matplotlib.pyplot as plt
-# import seaborn as sns
+from matplotlib import *
+import networkx as nx
 
 
-# x = [1,2,3,4,5,6]
-# y = [5,6,1,2,4,6]
+g = nx.Graph()
 
-# ax = sns.stripplot(x,y)
-# ax.set(xlabel = 'X axis', ylabel = 'Y axis')
 
-# plt.title('My first graph')
-# plt.show()
+
+
+
+
+
