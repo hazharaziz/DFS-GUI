5b482eab4c1b6db7698be25bbb74b5da8acdfb4c
Hazhar Aziz <hazharaziz5555@gmail.com>
2019-06-25 23:56:46 +0430

    Button Class implemented

--- Button.py
+++ Button.py
@@ -17,3 +17,18 @@ class Button:
         if outline:
             pygame.draw.rect(self.window,outline,(self.x - 2,self.y - 2,self.width + 4,self.height + 4))
         pygame.draw.rect(self.window, self.color, (self.x, self.y, self.width, self.height))
+
+        if self.text != '':
+            font = pygame.font.SysFont('Times New Roman',25)
+            text = font.render(self.text,1,(0,0,0))
+            self.window.blit(text,(self.x + (self.width/2 - text.get_width()/2), self.y + (self.height/2 - text.get_height()/2)))
+
+
+    def isOver(self,pos):
+        if pos[0] > self.x and pos[0] < self.x + self.width:
+            if pos[1] > self.y and pos[1] < self.y + self.height:
+                return True
+
+        return False
+
+
--- DFS-GUI.py
+++ DFS-GUI.py
@@ -4,54 +4,62 @@ import pygame.event as pe
 from Node import *
 from Button import *
 
-pygame.init()
+def main():
+    pygame.init()
 
-window_size = (500,500)
-window = pygame.display.set_mode(window_size)
-pygame.display.set_caption("Pygame")
+    window_size = (800, 500)
+    window = pygame.display.set_mode(window_size)
+    pygame.display.set_caption("DFS GUI")
 
-black = (0,0,0)
-white = (255,255,255)
+    black = (0, 0, 0)
+    white = (255, 255, 255)
 
-# node = Node(window, white)
-
-node = None
-
+    node = None
 
 
-button = Button()
-
-nodes = []
-
-while True:
-    window.fill(black)
-
-    node = Node(window,white)
-
-
-    for event in pe.get():
-        if event.type == pl.QUIT:
-            pygame.quit()
-            sys.exit()
-
-        if event.type == pl.MOUSEBUTTONDOWN:
-            node.pos = pygame.mouse.get_pos()
-            node.display = True
-            nodes.append(node)
-
-    for i in range(len(nodes)):
-        if nodes[i].display:
-            nodes[i].draw()
-
-    # if node.display:
-    #     node.draw()
-
-    pygame.display.update()
+    button1 = Button(window,(0,255,0),20,20,160,40,'AddNode')
+    button2 = Button(window,(0,255,0),220,20,160,40,'AddEdge')
+    button3 = Button(window,(0,255,0),420,20,160,40,'DFS Traversal')
+    button4 = Button(window,(0,255,0),620,20,160,40,'Reset')
 
 
 
+    nodes = []
+
+    while True:
+        window.fill(black)
 
 
+        button1.draw(white)
+        button2.draw(white)
+        button3.draw(white)
+        button4.draw(white)
+
+        node = Node(window,white)
+
+
+        for event in pe.get():
+            if event.type == pl.QUIT:
+                pygame.quit()
+                sys.exit()
+
+            if event.type == pl.MOUSEBUTTONDOWN:
+                node.pos = pygame.mouse.get_pos()
+                node.display = True
+                nodes.append(node)
+
+        for i in range(len(nodes)):
+            if nodes[i].display:
+                nodes[i].draw()
+
+        # if node.display:
+        #     node.draw()
+
+        pygame.display.update()
+
+
+if __name__ == '__main__':
+    main()
 
 
 
