85b227d711fe694915be4971665e69c34995380d
babak-ssh <babak.sefidgar@gmail.com>
2019-10-18 02:07:24 +0330

    modified by removing the loop inside AddNode

--- AddNode.py
+++ AddNode.py
@@ -20,49 +20,22 @@ def create_node(node, pos, i):
     node.display = True
 
 # add_node func for drawing the nodes on the window
-def add_node(window, buttons, colors, graph, nodes, edges, i):
+def add_node(window, buttons, colors, graph, nodes, edges, i, event):
+    node = Node(window, colors['green'])
+    pos = pygame.mouse.get_pos()
 
-    node = None
-
-    while True:
-
-        window.fill(colors['paper'])
-        pygame.draw.line(window, colors['emerald'], (20, 70), (780, 70))
-        Handle_Button.draw_btns(buttons, colors['emerald'])
-        node = Node(window, colors['green'])
-        pos = pygame.mouse.get_pos()
-
-        # event handling
-        for event in pe.get():
-
-            if event.type == pl.QUIT:
-                pygame.quit()
-                sys.exit()
-
-            if event.type == pl.KEYDOWN:
-                if event.key == pl.K_ESCAPE:
-                    pygame.quit()
-                    sys.exit()
-
-            if event.type == pl.MOUSEBUTTONDOWN:
-                if pos[1] > 90:
-                    if len(nodes) != 0:
-                        if not any([collide(pos, x) for x in nodes]):
-                            create_node(node, pos, i)
-                            nodes.append(node)
-                            i += 1
-                    else:
-                        create_node(node, pos, i)
-                        nodes.append(node)
-                        i += 1
-                Handle_Button.button_handler(window, buttons, colors, graph, nodes, edges, i, pos)
-
-            if event.type == pl.MOUSEMOTION:
-                for button in buttons:
-                    if button.isOver(pos):
-                        button.color = colors['silver']
-                    else:
-                        button.color = colors['blacksteel']
+    # event handling
+    if event.type == pl.MOUSEBUTTONDOWN:
+        if pos[1] > 90:
+            if len(nodes) != 0:
+                if not any([collide(pos, x) for x in nodes]):
+                    create_node(node, pos, i)
+                    nodes.append(node)
+                    i += 1
+            else:
+                create_node(node, pos, i)
+                nodes.append(node)
+                i += 1
 
         graph.nodes = nodes
         graph.edges = edges
