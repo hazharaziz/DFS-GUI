5e0ed9bd4ab37b21dd6dbd39b8f81416d0b32b4a
babak-ssh <babak.sefidgar@gmail.com>
2019-10-18 15:22:38 +0330

    removed the loop and used current_edge

--- AddEdge.py
+++ AddEdge.py
@@ -8,50 +8,28 @@ from Graph import *
 import Handle_Button 
 
 # add_edge func for drawing the edges on the screen
-def add_edge(window, buttons, colors, graph, nodes, edges, i):
+def add_edge(window, buttons, colors, graph, nodes, edges, i, event):
 
     start_node = None
     edge = None
 
-    while True:
-        window.fill(colors['paper'])
-        pygame.draw.line(window, colors['emerald'], (20, 70), (780, 70))
-        pos = pygame.mouse.get_pos()
-        edge = Edge(window, colors['blacksteel'])
-        Handle_Button.draw_btns(buttons, colors['emerald'])
+    pos = pygame.mouse.get_pos()
+    edge = Edge(window, colors['blacksteel'])
 
-        # event handling
-        for event in pe.get():
+    if event.type == pl.MOUSEBUTTONDOWN:
+        for node in nodes:
+            if node.isOver(pos):
+                if not graph.current_edge.start_node:
+                    graph.current_edge.start_node = node
+                    print("start")
+                elif not graph.current_edge.end_node:
+                    print("end")
+                    graph.current_edge.end_node = node
+                    graph.edges.append(graph.current_edge)
+                    graph.current_edge.start_node = Node(window, colors)
+                    graph.current_edge.end_node = Node(window, colors)
 
-            if event.type == pl.QUIT:
-                pygame.quit()
-                sys.exit()
-
-            if event.type == pl.KEYDOWN:
-                if event.key == pl.K_ESCAPE:
-                    pygame.quit()
-                    sys.exit()
-
-            if event.type == pl.MOUSEBUTTONDOWN:
-                for node in nodes:
-                    if node.isOver(pos):
-                        if start_node != None:
-                            edge.start_node = start_node
-                            edge.end_node = node
-                            edges.append(edge)
-                            start_node = None
-                        else:
-                            start_node = node
-                Handle_Button.button_handler(window, buttons, colors, graph, nodes, edges, i, pos)
-
-            if event.type == pl.MOUSEMOTION:
-                for button in buttons:
-                    if button.isOver(pos):
-                        button.color = colors['silver']
-                    else:
-                        button.color = colors['blacksteel']
-
-        graph.edges = edges
+        #graph.edges = edges
 
         # showing the graph
         graph.graph_show()
